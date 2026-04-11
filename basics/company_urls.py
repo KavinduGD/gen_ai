973a380bb5afb4e9e366efd4b2cef38e60aa11d6
Kavindu Gihan <kavidudharmasiri90@gmail.com>
2026-04-12 00:16:52 +0530

    project complete

---
+++ basics/company_urls.py
@@ -0,0 +1,155 @@
+urls = [
+    {
+        "name": "ws02",
+        "url": "https://wso2.com/careers/internships/"
+    },
+    {
+        "name": "gtn",
+        "url": "https://career44.sapsf.com/career?company=gtngroupho&career%5fns=job%5flisting%5fsummary&navBarLevel=JOB%5fSEARCH&_s.crb=nY%2fBf3gI9QeETIOjxOToFPzLZOrZJio%2baugDqNuFgJQ%3d"
+    },
+    {
+        "name": "creativesoftware",
+        "url": "https://www.creativesoftware.com/careers"
+    },
+    {
+        "name": "codegen",
+        "url": "https://codegen.co.uk/careers#"
+    },
+    {
+        "name": "gapstars",
+        "url": "https://gapstars.net/tech/jobs/"
+    },
+    {
+        "name": "lseg",
+        "url": "https://lseg.wd3.myworkdayjobs.com/Graduate_Careers?locationCountry=db69e062446c11de98360015c5e6daf6"
+    },
+    {
+        "name": "mileniumIT",
+        "url": "https://www.careers-page.com/mitesp#openings"
+    },
+    {
+        "name": "99x",
+        "url": "https://99x.io/careers/open-positions?location=sri-lanka"
+    },
+    {
+        "name": "syscolabs",
+        "url": "https://wd5.myworkdaysite.com/recruiting/sysco/syscocareers/jobs?locations=b014cc62fe6601b8d666502cd5287f36&workerSubType=6a5d7ea644cc10005042ccb105560002&workerSubType=b014cc62fe6601ac5a96d6b1da283744"
+    },
+    {
+        "name": "keellsIT",
+        "url": "https://careers.keells.com/JohnKeellsIT/go/Career-at-John-Keells-IT/516810/"
+    },
+    {
+        "name": "axiataDigitalLabs",
+        "url": "https://www.careers-page.com/axiata-digital-labs?page=1"
+    },
+    {
+        "name": "emojot",
+        "url": "https://emojot.com/emojot_mobile/mobile.jsp?shortUrl=emojotats&loginRequest=Anonymous&doKIOSKRequest=true&mobileSkin=emojotats_theme45#!#middle-container"
+    },
+    {
+        "name": "fortude",
+        "url": "https://careers.fortude.co/"
+    },
+    {
+        "name": "corzent",
+        "url": "https://careers.corzent.com/jobs"
+    },
+    {
+        "name": "acentura",
+        "url": "https://www.acentura.com/careers/index.php?page=1"
+    },
+    {
+        "name": "riskonnet",
+        "url": "https://www.acentura.com/careers/index.php?page=1"
+    },
+    {
+        "name": "codimite",
+        "url": "https://codimite.ai/careers/#openings-section"
+    },
+    {
+        "name": "altrium",
+        "url": "https://careers.us.sentiva.ai/altrium#positions"
+    },
+    {
+        "name": "zillione",
+        "url": "https://www.zillione.com/careers/#open-vacancies"
+    },
+    {
+        "name": "arimac",
+        "url": "https://arimac.digital/careers"
+    },
+    {
+        "name": "cambio",
+        "url": "https://careers.cambio.lk/jobs"
+    },
+    {
+        "name": "orelit",
+        "url": "https://orelit.com/join-us/"
+    },
+    {
+        "name": "zone24x7",
+        "url": "https://zone24x7.com/careers/#available-open-jobs"
+    },
+    {
+        "name": "inivos",
+        "url": "https://www.inivosglobal.com/careers"
+    },
+    {
+        "name": "tiqri",
+        "url": "https://hire.tiqri.com/"
+    },
+    {
+        "name": "vitalhub",
+        "url": "https://www.vitalhub.lk/jobs"
+    },
+    {
+        "name": "efuture",
+        "url": "https://www.efuturesworld.com/careers/"
+    },
+    {
+        "name": "octopusbi",
+        "url": "https://octopusbi.com/careers/"
+    },
+    {
+        "name": "jrcsoftware",
+        "url": "https://www.jrcsoftware.com/vacancies.html"
+    },
+    {
+        "name": "wiasystems",
+        "url": "https://www.wiasystems.com/careers"
+    },
+    {
+        "name": "digiratina",
+        "url": "https://www.digiratina.com/careers/"
+    },
+    {
+        "name": "redblocks",
+        "url": "https://redblocks.io/about-us/careers/"
+    },
+    {
+        "name": "flatrocktech",
+        "url": "https://flatrocktech.com/careers"
+    },
+    {
+        "name": "osos",
+        "url": "https://osos-obp-jobportal.osos.om/home?_gl=1*ioacck*_ga*MjA5MzAxMjQ4Mi4xNzczNzUyMDM4*_ga_4RHJF7026P*czE3NzM4MTI0MTckbzMkZzAkdDE3NzM4MTI0MTckajYwJGwwJGgw"
+    },
+    {
+        "name": "pickme",
+        "url": "https://pickme.lk/careers/#current-job-openings"
+    },
+    {
+        "name": "rootcode",
+        "url": "https://rootcode.io/careers"
+    },
+    {
+        "name": "mbizsoftware",
+        "url": "https://mbizsoftware.com/careers/"
+    },
+    {
+        "name": "circles",
+        "url": "https://circles.wd103.myworkdayjobs.com/Circles?locations=10d17f416b8b100207abea316bc70000"
+    },
+
+]
